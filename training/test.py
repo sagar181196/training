80d9982af9ad07d125a468ab58bea6d5fc4f92a9
apple <apple@apples-MBP-2.domain.name>
2022-03-04 10:21:47 +0530

    Training app added

---
+++ training/test.py
@@ -0,0 +1,18 @@
+N =input()
+x=[]
+for y in range(int(N)):
+    aa = raw_input()
+    action = aa.split(' ')
+    print(action)
+    # if action[0] == "print":
+    #     print(x)
+    # elif action[0]=="pop":
+    #     x.pop()
+    # elif action[0]=="sort":
+    #     x.sort()
+    # elif action[0]=="reverse":
+    #     x.sort(reverse=True)  
+    # elif action[0] == 'append':
+    #     x.append(int(action[1]))
+   
+    
---
+++ training/test2.py
@@ -0,0 +1,4 @@
+weight_pound = input("weight pound : ")
+weight_kg = int(weight_pound)*0.45
+
+print('your weight in kg {}'.format(weight_kg))
